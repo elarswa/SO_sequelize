17526c0420aeaf8c2b73006aa6434e2b35b7be21
Eric Larson <eric@gosolo.io>
2022-02-14 23:22:00 -0700

    fix path, add seeder

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const path = require('path');
 const basename = path.basename(__filename);
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: '../Music.db',
+  storage: './Music.db',
   logging: false,
 });
 const db = {};
@@ -31,7 +31,19 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
+const seed = async () => {
+  await Promise.all([
+    db.artist.create({ name: 'artist1' }),
+    db.artist.create({ name: 'artist2' }),
+    db.album.create({ name: 'album1', artist_id: 1 }),
+    db.playlist.create({ name: 'playlist1' }),
+  ]).catch(e => {});
+  await Promise.all([
+    db.track.create({ name: 'track1', artist_id: 1, album_id: 1 }),
+    db.track.create({ name: 'track2', artist_id: 1, album_id: 1 }),
+  ]).catch(e => {});
+};
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-// db.sequelize.sync({ force: true });
-db.sequelize.sync();
+db.sequelize.sync({ force: true }).then(() => seed());
